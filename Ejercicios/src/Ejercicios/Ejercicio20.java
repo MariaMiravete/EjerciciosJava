73d9b2e82cd651238421b5fc9f4e1a4474ae4dc2
Maria Miravete Herreria <miravete.aweb.96@gmail.com>
2016-10-25 18:59:29 +0200

    Ejercicio20

--- Ejercicios/src/Ejercicios/Ejercicio20.java
+++ Ejercicios/src/Ejercicios/Ejercicio20.java
@@ -6,37 +6,51 @@ public class Ejercicio20 { //class
 
 	public static void main(String[] args) { //main
 		Scanner teclado = new Scanner(System.in);
-		
-		String alumno"";
+
+		String alumno=("");
 		float nota1=0;
 		float nota2=0;
 		float nota3=0;
-		
+		float media=0;
+
+
 		//suma=0;
-		
+
 		System.out.println("Introduce el nombre:");
 		alumno=teclado.nextLine();
-		
-		
+
+
 		while (!alumno.equalsIgnoreCase("")) {
-		
-		System.out.println("dame una nota:");
-		nota1=teclado.nextFloat();
-		System.out.println("dame una nota:");
-		nota2=teclado.nextFloat();
-		System.out.println("dame una nota:");
-		nota3=teclado.nextFloat();
-		
-		System.out.println (0,1*nota1+0,);
-		
-		System.out.println("Introduce el nombre:");
-		alumno=teclado.nextLine();
-		
-		
+			System.out.println("dame una nota:");
+			nota1=teclado.nextFloat();
+			if (nota1<0 || (nota1>10)) {
+				System.out.println("error");
+
+			} else {
+				System.out.println("dame una nota:");
+				nota2=teclado.nextFloat();
+				if (nota2<0 || (nota2>10)) {
+					System.out.println("error");
+					
+				} else {
+					System.out.println("dame una nota:");
+					nota3=teclado.nextFloat();
+					
+					if (nota3<0 || (nota3>10)) {
+						System.out.println("error");
+					} else {
+						media=(float) (0.1*nota1+0.4*nota2+0.5*nota3);
+						System.out.println("Media "+media);
+					}
+				}
+				alumno=teclado.nextLine();				
+				System.out.println("Introduce el nombre:");
+				alumno=teclado.nextLine();
+			}
 		} //while		
-					 
-		
-		
+
+
+
 		//System.out.println (suma);
 	}//main
 } //class
