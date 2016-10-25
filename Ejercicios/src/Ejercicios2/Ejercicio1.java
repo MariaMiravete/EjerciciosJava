52ffc99ac0d080163b8d8b5fc8d20d1b04df51f9
Maria Miravete Herreria <miravete.aweb.96@gmail.com>
2016-10-25 19:24:29 +0200

    Ejercicio1(hoja2)

---
+++ Ejercicios/src/Ejercicios2/Ejercicio1.java
@@ -0,0 +1,15 @@
+package Ejercicios2;
+
+import java.util.Scanner;
+
+public class Ejercicio1 { //class
+
+	public static void main(String[] args) { //main
+		
+
+		for (int i=1; i<=100; i++) {
+			System.out.printf ("%3d \n", i);
+		}//for
+		
+	}//main
+} //class
